20f75992185643550e52fb2b8fd82662e1a9b99b
Tom Pantelis <tompantelis@gmail.com>
2023-08-02 12:23:53 -0400

    Use RequeueResource when service is re-created
    ...to re-queue the ServiceExport rather than calling
    serviceExportToServiceImport directly. This is safer and
    cleaner so that all ServiceExport notifications are handled
    from the same syncer.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- test/e2e/discovery/service_discovery.go
+++ test/e2e/discovery/service_discovery.go
@@ -30,6 +30,7 @@ import (
 	lhframework "github.com/submariner-io/lighthouse/test/e2e/framework"
 	"github.com/submariner-io/shipyard/test/e2e/framework"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -160,6 +161,19 @@ func RunServiceDiscoveryTest(f *lhframework.Framework) {
 		true, false)
 	verifySRVWithDig(f.Framework, framework.ClusterA, nginxServiceClusterB, netshootPodList, checkedDomains, "",
 		false, false)
+
+	By(fmt.Sprintf("Re-creating Nginx Service on %q", clusterBName))
+
+	nginxServiceClusterB.ObjectMeta = metav1.ObjectMeta{
+		Name:   nginxServiceClusterB.Name,
+		Labels: nginxServiceClusterB.Labels,
+	}
+	nginxServiceClusterB = f.CreateService(framework.KubeClients[framework.ClusterB].CoreV1().Services(f.Namespace), nginxServiceClusterB)
+	nginxServiceClusterB, err = f.GetService(framework.ClusterB, nginxServiceClusterB.Name, nginxServiceClusterB.Namespace)
+	Expect(err).NotTo(HaveOccurred())
+
+	f.VerifyServiceIPWithDig(framework.ClusterA, framework.ClusterB, nginxServiceClusterB, netshootPodList, checkedDomains,
+		"", true)
 }
 
 func RunServiceDiscoveryLocalTest(f *lhframework.Framework) {
